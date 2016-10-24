ca1b290678121da685b4002df0aaa023a19c7802
Michael Devery <michaeldevery@gmail.com>
2016-10-24 12:08:18 +0100

    add phpmd, phpcs to default php7 build. add sshd as cmd

---
+++ php7/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run -it \
+    -p 22:22 \
+    -p 80:80 \
+    -p 3306:3306 \
+    -v /home/michael/workspace:/var/www \
+    --network dockernet \
+    --ip 172.18.0.2 \
+    mickadoo/php7
